c12576c38daff43e5844d52ab36969147a466f4e
Even Holthe <even.holthe@me.com>
2013-10-21 13:48:20 +0200

    La til kommentar på toppen, som skal være med i alle klasser og la til en newline

--- ArbeidskravDriver.java
+++ ArbeidskravDriver.java
@@ -1,6 +1,14 @@
+/*
+ * Oblig 3 (DAPE1400) - forfattet av følgende
+ * studenter fra Anvendt datateknologi, 2. året:
+ * 
+ * Vegar Norman <s189153@stud.hioa.no> 
+ * Per Erik Finstad <s189138@stud.hioa.no>
+ * Even Holthe <s189124@stud.hioa.no>
+ */
 public class ArbeidskravDriver {
 
-	// Konstruktør.
+	// Konstruktør
 	public ArbeidskravDriver() {
 		
 	}
