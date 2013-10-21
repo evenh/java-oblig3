ae7a761781a64fcbc2653e74795df65838a0d2a9
NothingButTheHoff <pererikfinstad@Per-Erik-Finstads-MacBook-Pro-2.local>
2013-10-21 14:00:39 +0200

    Opprettet klasse ObligRegister. Fixes #13

---
+++ ObligRegister.java
@@ -0,0 +1,17 @@
+/*
+ * Oblig 3 (DAPE1400) - forfattet av følgende
+ * studenter fra Anvendt datateknologi, 2. året:
+ * 
+ * Vegar Norman <s189153@stud.hioa.no> 
+ * Per Erik Finstad <s189138@stud.hioa.no>
+ * Even Holthe <s189124@stud.hioa.no>
+ */
+public class ObligRegister{
+	private Student[] studenter;
+	private int antallObliger;
+
+	//Konstruktør
+	public ObligRegister(int antStud, int antOblig){
+		//kode her..
+	}
+}
