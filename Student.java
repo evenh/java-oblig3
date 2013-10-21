a88853a41951524a90edfb08bfaf821118775fe1
Even Holthe <even.holthe@me.com>
2013-10-21 13:34:17 +0200

    La til første utkast av Student-klassen

---
+++ Student.java
@@ -0,0 +1,11 @@
+/*
+ * Oblig 3 (DAPE1400) - forfattet av følgende
+ * studenter fra Anvendt datateknologi, 2. året:
+ * 
+ * Vegar Norman <s189153@stud.hioa.no> 
+ * Per Erik Finstad <s189138@stud.hioa.no>
+ * Even Holthe <s189124@stud.hioa.no>
+ */
+public class Student {
+
+}
