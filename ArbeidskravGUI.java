02860109c0683bd2fa369a8da911801b33979174
Vegar Norman <vegar@theinternetloves.me>
2013-10-23 09:28:13 +0200

    Lagt til beskrivende kommentarer

--- ArbeidskravGUI.java
+++ ArbeidskravGUI.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 
 public class ArbeidskravGUI extends JFrame implements ActionListener {
 
+
+	// Attributter
+	//============
+
 	private JTextField 	person,
 							 	obligNr,
 							 	kl;
@@ -22,38 +26,49 @@ public class ArbeidskravGUI extends JFrame implements ActionListener {
 	private JCheckBox 	godkjent;
 
 
+	// Konstruktør
+	//============
 
 	public ArbeidskravGUI(String fag, int antStudenter, int antObliger, int antPrGruppe) {
 
+		// Konstruktør arvet fra JFrame
 		super (fag + " - Arbeidskrav");
 
+		// Opprett nytt ObligRegistert basert på informasjon fra konstruktør
 		this.kartotek = new ObligRegister(antStudenter, antObliger);
 
+		// Opprett JTextField-objekter
 		this.person = new JTextField(10);
 		this.kl = new JTextField(10);
 		this.obligNr = new JTextField(1);
 
+		// Opprett JTextField[]-arrayer
 		this.navn = new JTextField[antPrGruppe];
 		this.klasse = new JTextField[antPrGruppe];
 
+		// Fyll JTextField[]-arrayer med JTextField-objekter
 		for (int i = 0; i < this.navn.length; i++) {
 			navn[i] = new JTextField(20);
 			klasse[i] = new JTextField(5);
 		}
 
+		// Opprett JButton, JCheckBox og JTextArea-objekter
 		this.reg = new JButton("Innlevering");
 		this.sjekk = new JButton("Sjekk godkjenning for student: ");
 		this.status = new JButton("Liste over godkjente i klasse: ");
 		this.godkjent = new JCheckBox("Godkjent");
 		this.output = new JTextArea(35, 35);
 
+		// Lag lyttere til JButton-objektene
 		this.reg.addActionListener(this);
 		this.sjekk.addActionListener(this);
 		this.status.addActionListener(this);
 
+		// Lag en Container med FlowLayout
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
 
+		// Legg alle objekter i JTextField[]-arrayene til Containeren
 		for (int i = 0; i < this.navn.length; i++) {
 			c.add(new JLabel("Navn: "));
 			c.add(navn[i]);
@@ -61,6 +76,7 @@ public class ArbeidskravGUI extends JFrame implements ActionListener {
 			c.add(klasse[i]);
 		}
 
+		// Legg alle andre objekter til Containeren
 		c.add(new JLabel("Obligatorisk oppgave nr: "));
 		c.add(this.obligNr);
 		c.add(this.godkjent);
@@ -71,6 +87,7 @@ public class ArbeidskravGUI extends JFrame implements ActionListener {
 		c.add(this.kl);
 		c.add(new JScrollPane(output));
 
+		// Innstillinger for JFrame
 		setSize(420, 800);
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
