a2dd3021ea93d0b587a2aacfe46c31673854fc28
Even Holthe <even.holthe@me.com>
2013-10-26 23:27:07 +0200

    La til kommentarer om opphavsmennene

--- ArbeidskravGUI.java
+++ ArbeidskravGUI.java
@@ -1,3 +1,12 @@
+/*
+ * Oblig 3 (DAPE1400) - forfattet av følgende
+ * studenter fra Anvendt datateknologi, 2. året:
+ * 
+ * Vegar Norman <s189153@stud.hioa.no> 
+ * Per Erik Finstad <s189138@stud.hioa.no>
+ * Even Holthe <s189124@stud.hioa.no>
+ */
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
