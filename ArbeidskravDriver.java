da190046a9064219f7ae705f4dcef7f13f12b0e1
Even Holthe <even.holthe@me.com>
2013-10-26 22:29:29 +0200

    Driverprogrammet (main) opprettet and workin'. Fixes #20

--- ArbeidskravDriver.java
+++ ArbeidskravDriver.java
@@ -6,11 +6,14 @@
  * Per Erik Finstad <s189138@stud.hioa.no>
  * Even Holthe <s189124@stud.hioa.no>
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+
 public class ArbeidskravDriver {
-
-	// Konstruktør
-	public ArbeidskravDriver() {
-		
+	public static void main(String[] args) {
+		ArbeidskravGUI ak = new ArbeidskravGUI("Programmering (DAPE1400)", 3, 4, 3);
 	}
-
 }
